2a532779e9e2d4192a86be32b4e4d17a8e89f0b4
Hector Alvarado <hectoralvaradocampos83@gmail.com>
2021-07-17 12:10:09 -0400

    Lombokeao
    lombokeando

--- src/main/java/com/tomas/cinturonnegro/models/ModeloBase.java
+++ src/main/java/com/tomas/cinturonnegro/models/ModeloBase.java
@@ -1,7 +1,12 @@
 package com.tomas.cinturonnegro.models;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
+@Getter @Setter @NoArgsConstructor
 public abstract class ModeloBase {
 }
