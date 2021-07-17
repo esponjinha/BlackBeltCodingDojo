24595ef96b6c9de9111341825221e700ba9ed366
Henrry <hectorroyaleesponja@gmail.com>
2021-07-17 12:15:00 -0400

    ModeloBase
    ModeloBase

--- src/main/java/com/tomas/cinturonnegro/models/ModeloBase.java
+++ src/main/java/com/tomas/cinturonnegro/models/ModeloBase.java
@@ -3,10 +3,32 @@ package com.tomas.cinturonnegro.models;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
+import java.util.Date;
 
 @MappedSuperclass
 @Getter @Setter @NoArgsConstructor
 public abstract class ModeloBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(updatable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date createdAt;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    public Date updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = new Date();
+    }
 }
--- src/main/java/com/tomas/cinturonnegro/repositories/RepositorioBase.java
+++ src/main/java/com/tomas/cinturonnegro/repositories/RepositorioBase.java
@@ -1,7 +1,11 @@
 package com.tomas.cinturonnegro.repositories;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
+import java.util.List;
+
 @NoRepositoryBean
-public interface RepositorioBase {
+public interface RepositorioBase<T> extends CrudRepository<T, Long> {
+    List<T> findAll();
 }
