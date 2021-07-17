57b9c93c678ddb2f041c5f55805c25458e931d64
Henrry <henrrydanielmejia@gmail.com>
2021-07-17 11:54:53 -0400

    cinturon negro

---
+++ src/main/java/com/tomas/cinturonnegro/CinturonNegroApplication.java
@@ -0,0 +1,13 @@
+package com.tomas.cinturonnegro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinturonNegroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinturonNegroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tomas/cinturonnegro/CinturonNegroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomas.cinturonnegro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinturonNegroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
